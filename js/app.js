e07b6dbb12753a2ded895b59f3d41313effe4b88
mirandaklein <mirandaklein@mac.com>
2018-01-17 09:59:57 -0700

    star functionality

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@ let cardDeck = document.getElementById('deck-of-cards');
  */
 let moves = 0;
 let movesCounter = document.getElementsByClassName('moves');
-//console.log(movesCounter);
+console.log(movesCounter);
+
+
+
 var openCards = [];
 /*
  * Display the cards on the page
@@ -67,6 +70,8 @@ function showCards() {
 
     for (card of cardList) {
         card.addEventListener('click', function () {
+            console.log(this);
+            addMoves();
             if (!cardOne) {
                 cardOne = this;
                 this.classList.toggle('open');
@@ -104,7 +109,25 @@ function showCards() {
         })
     }
 };
-showCards();
+
+
+function addMoves() {
+    moves++;
+    movesCounter[0].innerText = moves;
+    if (moves === 1){
+        removeStar();
+    }
+
+};
+
+//Currently removeStar is running before 5 clicks can be accumulated
+
+function removeStar(){
+    document.getElementById('starsTwo').classList.remove('fa-star')
+};
+    showCards();
+
+    
 
 
 
@@ -120,13 +143,13 @@ showCards();
 
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    /*
+     * set up the event listener for a card. If a card is clicked:
+     *  - display the card's symbol (put this functionality in another function that you call from this one)
+     *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+     *  - if the list already has another card, check to see if the two cards match
+     *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+     *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+     *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+     *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+     */
